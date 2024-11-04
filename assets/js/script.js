526c7d985e24bd5da23073fc3e7574fe5bc2128f
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-04 13:51:46 +0000

    Add displayExceptions function - displays errors in Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,6 +53,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -97,6 +98,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -113,3 +115,16 @@ function displayStatus(data) {
     resultsModal.show();
 }
 
+function displayException(data) {
+
+    let heading = "An Exception Occured";
+    let results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div> Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div> Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+
+}
