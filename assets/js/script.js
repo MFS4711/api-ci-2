07dce69a900c183a0c2286a3a579f5c997afbc6d
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-04 12:41:51 +0000

    Add post method and function for API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,56 @@
+// Constants for API and modal
 const API_KEY = "R8aumRoNREx88_2W2p84eO-vU5w";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
+// get data
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+// post data
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    // below may be specific to API but follow instructions on the API
+    // use body: form, attaches the form
+    const response = await fetch(API_URL, {
+                                method: "POST",
+                                headers: {
+                                        "Authorization": API_KEY,
+                                },
+                                body: form,
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors(data) {
+
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+
+}
 
 // async functions can be used as opposed to .then - wrap promises in this
 // use fetch for the api url - used queryString because of variable used
@@ -30,10 +78,11 @@ function displayStatus(data) {
 
     let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
-    results += `<div class="key-status">${data.expiry}</div>`
+    results += `<div class="key-status">${data.expiry}</div>`;
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
 
     resultsModal.show();
 }
+
